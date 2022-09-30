9c0bf0dce3899bb9b7b4e1860bba44c93d51e3ab
Carlos Gámez <hardpx@gmail.com>
2022-09-29 20:25:27 -0500

    add client side js to fetch the url

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", alert("cliked"));
+btn.addEventListener("click", apiRequest());
+
+async function apiRequest() {
+  const rapperName = document.querySelector("input").value;
+  try {
+    const response = await fetch(`url/${rapperName}`);
+    const data = await response.json();
+  } catch (err) {
+    console.log(`We found the error ${err}`);
+  }
+}
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 //create a variable to hold expresss, and let us use all of express methods
 const app = express();
 //cors is a npm package (module) which enables controlled access to resources located outside of a given domain, like local file requests... we need to install cors => npm install cors --save
+//cors let hosted servers hae access to local files (main.js)
 const cors = require("cors");
 //create a PORT varible to save a number so we can listen
 const PORT = 8000;
@@ -48,6 +49,7 @@ app.get("/api/:rapperName", (request, response) => {
   //response.json(rappers)
 });
 
+//this let us use the port the hosting is making us use and if its not available use our PORT
 app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is running on PORT ${PORT}! YOU BETTER GO CATCH IT!`);
 });
